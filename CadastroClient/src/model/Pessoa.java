4e42c487b32a006d9632d463ba8298ca17a3d5a0
Fabio Lazaro de Almeida <fabio@adbits.com.br>
2024-06-15 00:24:28 -0300

    removido comentários

--- CadastroClient/src/model/Pessoa.java
+++ CadastroClient/src/model/Pessoa.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 import java.io.Serializable;
@@ -18,10 +14,6 @@ import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-/**
- *
- * @author rbgor
- */
 @Entity
 @Table(name = "Pessoa")
 @NamedQueries({
